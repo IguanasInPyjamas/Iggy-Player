3f9eadf725db3d0097300681bf33e0c8c0366baa
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-12-17 23:53:27 +0000

    org

--- src/stream_info.rs
+++ src/stream_info.rs
@@ -1,3 +1,7 @@
+use gstreamer;
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use gstreamer::prelude::*;
+use glib;
 
 pub struct _StreamInfo {
     pub playbin: gstreamer::Element, //Stream.
@@ -14,4 +18,16 @@ pub struct _StreamInfo {
     pub current_subtitle: i32,
 
     pub mute: bool,
+}
+
+pub fn populate_from_playbin(stream_info: &mut _StreamInfo){
+    stream_info.n_audio_streams = stream_info.playbin.get_property("n-audio").unwrap().get_some::<i32>().unwrap();
+    stream_info.n_video_streams = stream_info.playbin.get_property("n-video").unwrap().get_some::<i32>().unwrap();
+    stream_info.n_subtitles = stream_info.playbin.get_property("n-text").unwrap().get_some::<i32>().unwrap();
+
+    stream_info.current_audio_stream = stream_info.playbin.get_property("current-audio").unwrap().get_some::<i32>().unwrap();
+    stream_info.current_video_stream = stream_info.playbin.get_property("current-video").unwrap().get_some::<i32>().unwrap();
+    stream_info.current_subtitle = stream_info.playbin.get_property("current-text").unwrap().get_some::<i32>().unwrap();
+    println!("{}, {}, {}", stream_info.current_audio_stream, stream_info.current_video_stream, stream_info.current_subtitle);
+
 }
