8042c401f14d679205d5876eb3119da316456f5d
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-12-18 00:25:12 +0000

    Update inputs.rs

--- src/inputs.rs
+++ src/inputs.rs
@@ -7,14 +7,14 @@ use crate::stream_info;
 pub fn seek_right(stream_info: &mut stream_info::_StreamInfo, prev_position: gstreamer::ClockTime) -> gstreamer::ClockTime {
     let mut previous = prev_position;
     stream_info.playbin.seek_simple(gstreamer::SeekFlags::FLUSH | gstreamer::SeekFlags::KEY_UNIT, previous + 5 * gstreamer::SECOND).expect("Failed to seek");
-    previous = previous + 5 * gstreamer::SECOND;
+    previous += 5 * gstreamer::SECOND;
     return previous;
 }
 
 pub fn seek_left(stream_info: &mut stream_info::_StreamInfo, prev_position: gstreamer::ClockTime) -> gstreamer::ClockTime {
     let mut previous = prev_position;
     stream_info.playbin.seek_simple(gstreamer::SeekFlags::FLUSH | gstreamer::SeekFlags::KEY_UNIT, prev_position - 5 * gstreamer::SECOND).expect("Failed to seek");
-    previous = previous - 5 * gstreamer::SECOND;
+    previous -= 5 * gstreamer::SECOND;
     return previous
 
 }
@@ -39,9 +39,13 @@ pub fn switch_audio(stream_info: &mut stream_info::_StreamInfo){
     stream_info.current_audio_stream = new_stream;
 }
 
-//fn switch_subs(stream_info){
-//    0;
-//}
+pub fn switch_subs(stream_info: &mut stream_info::_StreamInfo){
+    //BUG: When the subs go they don't come back
+    let new_stream = (stream_info.current_subtitle + 1) % stream_info.n_subtitles;
+    stream_info.playbin.set_property("current-text", &new_stream).unwrap();
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    stream_info.current_audio_stream = new_stream;
+}
 
 //fn toggle_subs(stream_info){
 //    0;
