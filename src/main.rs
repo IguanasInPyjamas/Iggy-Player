dc41dea182c0dc8126b6caf5c78de94016002400
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-12-18 00:24:07 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use glib;
 
 pub mod stream_info;
 pub mod inputs;
+pub mod events;
 
 
 fn main() {
@@ -23,15 +24,8 @@ fn main() {
 
     pipeline.set_state(gstreamer::State::Playing).expect("Unable to set to playing");
 
-    //Using flags makes this all giga easy, we can query the names and values from the flags, hopefully to dynamically make a list of audio/sub streams
-    let flags = pipeline.get_property("flags").unwrap();
-    let flags_class = glib::FlagsClass::new(flags.type_()).unwrap();
-    //These can then be set and unset using this value.
-    let flags = flags_class.builder_with_value(flags).unwrap()
-        .set_by_nick("audio")
-        .build()
-        .unwrap();
-    pipeline.set_property("flags", &flags).unwrap();
+
+
     let bus = pipeline.get_bus().unwrap();
 
     let mut prev_position = 0 * gstreamer::SECOND;
@@ -51,53 +45,29 @@ fn main() {
 
     };
 
-    stream_info.playbin
-        .connect("audio-changed", false, |values| {
-            let playbin = values[0]
-                .get::<glib::Object>()
-                .expect("playbin \"audio-tags-changed\" signal values[1]")
-                .unwrap();
+    let (flags, flags_class) = events::setup_flags_class(&stream_info);
+    //These can then be set and unset using this value.
 
-            let a = playbin.get_property("current-audio").unwrap().get_some::<i32>().unwrap();
-            println!("hmm {:?}", a);
 
-            None
-        })
+    let flags = flags_class.builder_with_value(flags).unwrap()
+        .set_by_nick("audio")
+        .build()
         .unwrap();
+    stream_info.playbin.set_property("flags", &flags).unwrap();
 
-    stream_info.playbin
-        .connect("audio-tags-changed", false, |values| {
-            let playbin = values[0]
-                .get::<glib::Object>()
-                .expect("playbin \"audio-changed\" signal values[1]")
-                .unwrap();
-
-            let idx = values[1]
-                .get_some::<i32>()
-                .expect("playbin \"audio-changed\" signal values[1]");
-
-            let tags = playbin.emit("get-audio-tags", &[&idx]).unwrap().unwrap();
-            let tags = tags.get::<gstreamer::TagList>().expect("tags").unwrap();
-
-            if let Some(artist) = tags.get::<gstreamer::tags::LanguageCode>() {
-                println!("  Language changed to : {}", artist.get().unwrap());
-            }
-
-            None
-        })
-        .unwrap();
+    events::setup_connects(&stream_info);
 
     //TODO: Sleep is here to ensure that the video is buffered before the streams are queried, we can probably get this from a signal return instead of time.
     std::thread::sleep(std::time::Duration::from_millis(4000));
     stream_info::populate_from_playbin(&mut stream_info);
-    
+
     while !stream_info.terminate {
         let msg = bus.timed_pop(25 * gstreamer::MSECOND);
         let keys: Vec<Keycode> = device_state.get_keys();
 
         match msg {
             Some(msg) => {
-                handle_message(&mut stream_info, &msg);
+                events::handle_message(&mut stream_info, &msg);
             }
             None => {
                 if keys.contains(&Keycode::Space) {
@@ -150,6 +120,10 @@ fn main() {
                 if keys.contains(&Keycode::M) {
                     inputs::mute(&mut stream_info);
                 }
+
+                if keys.contains(&Keycode::S){
+                    inputs::switch_subs(&mut stream_info);
+                }
             }
         }
     }
@@ -157,54 +131,4 @@ fn main() {
 
 
 
-}
-
-fn handle_message(stream_info: & mut stream_info::_StreamInfo, msg: & gstreamer::Message) {
-    use gstreamer::MessageView;
-
-    match msg.view(){
-        MessageView::Error(err)=>{
-            println!(
-                "Error received from element {:?}: {} ({:?})",
-                err.get_src().map(|s| s.get_path_string()),
-                err.get_error(),
-                err.get_debug()
-            );
-            stream_info.terminate = true;
-        }
-        MessageView::DurationChanged(_) => {
-            stream_info.duration = gstreamer::CLOCK_TIME_NONE;
-        }
-        MessageView::Eos(..) => {
-            println!("End of stream reached.");
-            stream_info.terminate = true;
-        }
-        MessageView::StateChanged(state_changed) => {
-            if state_changed
-                .get_src()
-                .map(|s| s == stream_info.playbin)
-                .unwrap_or(false){
-
-                let new_state = state_changed.get_current();
-                let old_state = state_changed.get_old();
-                println!("State changed from {:?} to {:?}",old_state,new_state);
-
-                stream_info.playing = new_state == gstreamer::State::Playing;
-                if stream_info.playing{
-                    let mut seeking = gstreamer::query::Seeking::new(gstreamer::Format::Time);
-                    if stream_info.playbin.query(&mut seeking){
-                        let (seekable, start, end) = seeking.get_result();
-                        stream_info.seek_enabled = seekable;
-                        if seekable {
-                            println!("Seeking is enabled");
-                        } else {
-                            println!("Seeking is disabled");
-                        }
-                    } else {
-                        eprintln!("query failed")
-                    }
-                }
-            }
-        } _ => (),
-    }
 }
