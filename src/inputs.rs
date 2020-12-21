f4c8c88e80a8861328c4ac4e81ac77985d4bf33d
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-12-21 12:44:58 +0000

    Update inputs.rs

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,6 +1,8 @@
 use gstreamer;
 use gstreamer::prelude::*;
-use glib;
+use device_query::Keycode;
+use std::io;
+use std::io::Write;
 
 use crate::stream_info;
 
@@ -21,12 +23,12 @@ pub fn seek_left(stream_info: &mut stream_info::_StreamInfo, prev_position: gstr
 
 pub fn mute(stream_info: &mut stream_info::_StreamInfo){
     if stream_info.mute == true {
-        stream_info.playbin.set_property("mute", &false);
+        stream_info.playbin.set_property("mute", &false).expect("Property could not be set");
         std::thread::sleep(std::time::Duration::from_millis(200));
         stream_info.mute = false;
     } else
     {
-        stream_info.playbin.set_property("mute", &true);
+        stream_info.playbin.set_property("mute", &true).expect("Property could not be set");
         std::thread::sleep(std::time::Duration::from_millis(200));
         stream_info.mute = true;
     }
@@ -44,7 +46,7 @@ pub fn switch_subs(stream_info: &mut stream_info::_StreamInfo){
     let new_stream = (stream_info.current_subtitle + 1) % stream_info.n_subtitles;
     stream_info.playbin.set_property("current-text", &new_stream).unwrap();
     std::thread::sleep(std::time::Duration::from_millis(50));
-    stream_info.current_subtitle = new_stream;
+    stream_info.current_audio_stream = new_stream;
 }
 
 //fn toggle_subs(stream_info){
@@ -61,4 +63,69 @@ pub fn pause(stream_info: &stream_info::_StreamInfo){
             .expect("Could not resume");
     }
 
+}
+
+pub fn check_keypress(keys: &Vec<device_query::Keycode>,position:gstreamer::ClockTime,mut stream_info: &mut stream_info::_StreamInfo) -> gstreamer::ClockTime {
+    let mut prev_position = position;
+    if keys.contains(&Keycode::Space) {
+        pause(&stream_info);
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+
+    if keys.contains(&Keycode::A) &&
+        stream_info.seek_enabled && !stream_info.playing {
+        prev_position = seek_left(&mut stream_info, prev_position);
+
+    }
+
+    if keys.contains(&Keycode::D) &&
+        stream_info.seek_enabled && !stream_info.playing {
+        prev_position = seek_right(&mut stream_info, prev_position);
+
+    }
+    if stream_info.playing {
+        let position = stream_info
+            .playbin
+            .query_position::<gstreamer::ClockTime>().expect("Couldn't query");
+
+
+        if stream_info.duration == gstreamer::CLOCK_TIME_NONE {
+            stream_info.duration = stream_info.playbin.query_duration().expect("Couldn't query");
+        }
+        println!("Position {} / {}", position, stream_info.duration);
+        io::stdout().flush().unwrap();
+
+
+        if keys.contains(&Keycode::D) && stream_info.seek_enabled {
+            stream_info.playbin.set_state(gstreamer::State::Paused).expect("Could not set to paused");
+            prev_position = seek_right(&mut stream_info, prev_position);
+            stream_info.playbin.set_state(gstreamer::State::Playing).expect("Could not set to playing");
+            std::thread::sleep(std::time::Duration::from_millis(250))
+        }
+
+        if keys.contains(&Keycode::A) && stream_info.seek_enabled {
+            stream_info.playbin.set_state(gstreamer::State::Paused).expect("Could not set to paused");
+            prev_position = seek_left(&mut stream_info, prev_position);
+            stream_info.playbin.set_state(gstreamer::State::Playing).expect("Could not set to playing");
+            std::thread::sleep(std::time::Duration::from_millis(250))
+        }
+
+
+
+        if keys.contains(&Keycode::V) {
+            switch_audio(&mut stream_info);
+            prev_position = position;
+        }
+    }
+    if keys.contains(&Keycode::M) {
+        mute(&mut stream_info);
+        prev_position = position;
+    }
+
+    if keys.contains(&Keycode::S){
+        switch_subs(&mut stream_info);
+        prev_position = position;
+    }
+
+    return prev_position
 }
