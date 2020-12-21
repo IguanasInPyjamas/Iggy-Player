770870131f09aeed21a4fc42cfc0036a95d17701
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-12-21 12:48:21 +0000

    Update uri.rs

--- src/uri.rs
+++ src/uri.rs
@@ -1,5 +1,20 @@
-use std::path;
-use url::Url;
+
+pub fn setup_pipeline(path:&std::string::String) -> gstreamer::Element {
+    //let mut filepath :std::string::String = "".to_string();
+    //For use in unit tests.
+    //let path = "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_cropped_multilingual.webm";
+    //Relatively naive, needs to be future proofed
+    let mut filepath = std::string::String::from("");
+    if path.contains("https") || path.contains("http"){
+        path.replace(" ","%20");
+        filepath = path.to_string();
+    } else {
+        filepath = get_uri_from_path(&path)
+    };
+    let pipe = format!("playbin uri={} name=play video-sink='autovideosink' audio-sink='autoaudiosink'", filepath);
+    let pipeline = gstreamer::parse_launch(&pipe).unwrap();
+    return pipeline;
+}
 
 pub fn get_uri_from_path(filepath: &str) -> String {
     let path = std::path::Path::new(filepath);
