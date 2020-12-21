7583fb7a578bf1f9eb7acaa65745fc86f51c0e0e
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-12-21 13:22:12 +0000

    Naive handling for online video links (#8)
    * Update main.rs
    
    * Create player.rs
    
    * Update main.rs
    
    * Update events.rs
    
    * Update inputs.rs
    
    * Update main.rs
    
    * Update player.rs
    
    * Update stream_info.rs
    
    * Update uri.rs

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,24 @@
-use std::io;
-use std::io::Write;
-use gstreamer;
-use device_query::{DeviceQuery, DeviceState, Keycode};
-use gstreamer::prelude::*;
-use glib;
 
 pub mod stream_info;
 pub mod inputs;
 pub mod events;
 pub mod uri;
+pub mod player;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    //args 0 has the reference to binaries, the first filepath will then be in point 1
-    //TODO: Encorporate more use of args to play multiple files, preferably after a --playlist tag
-
-    let path = &args[1];
-    gstreamer::init().unwrap();
-    let device_state = DeviceState::new();
-    //TODO Implement a filepath to URI function and then this doesn't have to be hardcoded, this is just a test case.
-    //TODO Use the GNU online videos for unit testing purposes.
-    // TODO Implement unit tests.
-
-    //For use in unit tests.
-    //let test_path = ""https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_cropped_multilingual.webm";
-
-    let filepath = uri::get_uri_from_path(path);
-
-    let pipe = format!("playbin uri={} name=play video-sink='autovideosink' audio-sink='autoaudiosink'", filepath);
-
-
-
-    let mut pipeline = gstreamer::parse_launch(&pipe).unwrap();
-
-    pipeline.set_state(gstreamer::State::Playing).expect("Unable to set to playing");
-
-
-
-    let bus = pipeline.get_bus().unwrap();
-
-    let mut prev_position = 0 * gstreamer::SECOND;
-    let mut stream_info = stream_info::_StreamInfo {
-        playbin: pipeline,
-        playing: false,
-        terminate: false,
-        seek_enabled: false,
-        duration: gstreamer::CLOCK_TIME_NONE,
-        n_video_streams: 0,
-        n_audio_streams: 0,
-        n_subtitles: 0,
-        current_audio_stream: 0,
-        current_video_stream: 0,
-        current_subtitle: 0,
-        mute: false,
-
-    };
-
-    let (flags, flags_class) = events::setup_flags_class(&stream_info);
-    //These can then be set and unset using this value.
-
-
-    let flags = flags_class.builder_with_value(flags).unwrap()
-        .set_by_nick("audio")
-        .build()
-        .unwrap();
-    stream_info.playbin.set_property("flags", &flags).unwrap();
-
-    events::setup_connects(&stream_info);
-
-    //TODO: Sleep is here to ensure that the video is buffered before the streams are queried, we can probably get this from a signal return instead of time.
-    std::thread::sleep(std::time::Duration::from_millis(4000));
-    stream_info::populate_from_playbin(&mut stream_info);
-
-    while !stream_info.terminate {
-        let msg = bus.timed_pop(25 * gstreamer::MSECOND);
-        let keys: Vec<Keycode> = device_state.get_keys();
-
-        match msg {
-            Some(msg) => {
-                events::handle_message(&mut stream_info, &msg);
-            }
-            None => {
-                if keys.contains(&Keycode::Space) {
-                    inputs::pause(&stream_info);
-                    std::thread::sleep(std::time::Duration::from_millis(500));
-                }
-
-                if keys.contains(&Keycode::A) &&
-                    stream_info.seek_enabled && !stream_info.playing {
-                    prev_position = inputs::seek_left(&mut stream_info, prev_position);
-
-                }
-
-                if keys.contains(&Keycode::D) &&
-                    stream_info.seek_enabled && !stream_info.playing {
-                    prev_position = inputs::seek_right(&mut stream_info, prev_position);
-
-                }
-                if stream_info.playing {
-                    let mut position = stream_info
-                        .playbin
-                        .query_position::<gstreamer::ClockTime>().expect("Couldn't query");
-
-
-                    if stream_info.duration == gstreamer::CLOCK_TIME_NONE {
-                        stream_info.duration = stream_info.playbin.query_duration().expect("Couldn't query");
-                    }
-                    println!("Position {} / {}", position, stream_info.duration);
-                    io::stdout().flush().unwrap();
-
-
-                    if keys.contains(&Keycode::D) && stream_info.seek_enabled {
-                        stream_info.playbin.set_state(gstreamer::State::Paused).expect("Could not set to paused");
-                        prev_position = inputs::seek_right(&mut stream_info, prev_position);
-                        stream_info.playbin.set_state(gstreamer::State::Playing).expect("Could not set to playing");
-                    }
-
-                    if keys.contains(&Keycode::A) && stream_info.seek_enabled {
-                        stream_info.playbin.set_state(gstreamer::State::Paused).expect("Could not set to paused");
-                        prev_position = inputs::seek_left(&mut stream_info, prev_position);
-                        stream_info.playbin.set_state(gstreamer::State::Playing).expect("Could not set to playing");
-                    }
-
-                    prev_position = position;
-
-                    if keys.contains(&Keycode::V) {
-                        inputs::switch_audio(&mut stream_info);
-                    }
-                }
-                if keys.contains(&Keycode::M) {
-                    inputs::mute(&mut stream_info);
-                }
-
-                if keys.contains(&Keycode::S){
-                    inputs::switch_subs(&mut stream_info);
-                }
-            }
+    let args:Vec<String> = std::env::args().collect();
+    let arg_len = args.len();
+    if arg_len == 1{
+        println!("No video was selected!");
+    }
+    else if arg_len == 2{
+       player::player_loop(args);
+    }
+    else {
+        for i in 1..(arg_len){
+            let new_arg:Vec<String> = vec![args[0].clone(),args[i].clone()];
+            player::player_loop(new_arg);
+            std::thread::sleep(std::time::Duration::from_millis(1000));
         }
     }
-    stream_info.playbin.set_state(gstreamer::State::Null).expect("Unable to set state");
-
-
-
 }
--- src/stream_info.rs
+++ src/stream_info.rs
@@ -1,7 +1,5 @@
 use gstreamer;
-use device_query::{DeviceQuery, DeviceState, Keycode};
 use gstreamer::prelude::*;
-use glib;
 
 pub struct _StreamInfo {
     pub playbin: gstreamer::Element, //Stream.
@@ -29,5 +27,25 @@ pub fn populate_from_playbin(stream_info: &mut _StreamInfo){
     stream_info.current_video_stream = stream_info.playbin.get_property("current-video").unwrap().get_some::<i32>().unwrap();
     stream_info.current_subtitle = stream_info.playbin.get_property("current-text").unwrap().get_some::<i32>().unwrap();
     println!("{}, {}, {}", stream_info.current_audio_stream, stream_info.current_video_stream, stream_info.current_subtitle);
+    println!("{}, {}, {}", stream_info.n_audio_streams, stream_info.n_video_streams, stream_info.n_subtitles);
 
+}
+
+pub fn initialise_stream(pipeline:gstreamer::Element) -> _StreamInfo {
+    let stream_info = _StreamInfo {
+        playbin: pipeline,
+        playing: false,
+        terminate: false,
+        seek_enabled: false,
+        duration: gstreamer::CLOCK_TIME_NONE,
+        n_video_streams: 0,
+        n_audio_streams: 0,
+        n_subtitles: 0,
+        current_audio_stream: 0,
+        current_video_stream: 0,
+        current_subtitle: 0,
+        mute: false,
+
+    };
+    return stream_info
 }
