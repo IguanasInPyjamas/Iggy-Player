3418c14b06701e7cc9a421f559f015818934ff27
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-12-21 12:45:27 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use std::io;
-use std::io::Write;
-use gstreamer;
-use device_query::{DeviceQuery, DeviceState, Keycode};
-use gstreamer::prelude::*;
-use glib;
 
 pub mod stream_info;
 pub mod inputs;
@@ -14,13 +8,14 @@ pub mod player;
 fn main() {
     let args:Vec<String> = std::env::args().collect();
     let arg_len = args.len();
-    if args.len() == 1{
+    if arg_len == 1{
         println!("No video was selected!");
-    };
-    if args.len() == 2{
+    }
+    else if arg_len == 2{
        player::player_loop(args);
-    } else {
-        for i in 1..(args.len()){
+    }
+    else {
+        for i in 1..(arg_len){
             let new_arg:Vec<String> = vec![args[0].clone(),args[i].clone()];
             player::player_loop(new_arg);
             std::thread::sleep(std::time::Duration::from_millis(1000));
